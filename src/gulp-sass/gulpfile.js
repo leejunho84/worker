e006e7791b0f28a8114e4b25694336d6ab906f9d
leejunho <div51@naver.com>
2016-07-14 15:38:41 +0900

    readme update

--- src/gulp-sass/gulpfile.js
+++ src/gulp-sass/gulpfile.js
@@ -23,9 +23,9 @@ gulp.task('staticServer', ['build:sass'], function(){
 
 gulp.task('build:sass', function(){
 	return gulp.src(args.sass.src)
-		.pipe(sourcemaps.init({loadMaps: true, debug: true}))
-		.pipe(sass({outputStyle:"compressed"}).on('error', sass.logError))
-		.pipe(sourcemaps.write('./'))
+		//.pipe(sourcemaps.init({loadMaps: true, debug: true}))
+		.pipe(sass().on('error', sass.logError))
+		//.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('./css'))
 		.pipe(browserSync.reload({stream:true}))
 });
